1c3f51fc7aa3c1cad7358b18417095d7cbf812dd
Sudhanshu <sudhanshuloshali007@gmail.com>
2025-03-22 10:33:20 +0530

    Refactored update_quality into separate methods for each item type

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -14,33 +14,42 @@ class GildedRose:
     def update_quality(self):
         """Update quality and sell-in values for all items based on their type."""
         for item in self.items:
-            if item.name != self.AGED_BRIE and item.name != self.BACKSTAGE_PASSES:
-                if item.quality > 0:
-                    if item.name != self.SULFURAS:
-                        item.quality = item.quality - 1
+            if item.name == self.AGED_BRIE:
+                self._update_aged_brie(item)
+            elif item.name == self.BACKSTAGE_PASSES:
+                self._update_backstage_passes(item)
+            elif item.name == self.SULFURAS:
+                pass  # Sulfuras never changes
             else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == self.BACKSTAGE_PASSES:
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != self.SULFURAS:
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != self.AGED_BRIE:
-                    if item.name != self.BACKSTAGE_PASSES:
-                        if item.quality > 0:
-                            if item.name != self.SULFURAS:
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                self._update_normal_item(item)
+
+    def _update_normal_item(self, item):
+        """Update quality and sell-in for normal items."""
+        if item.quality > 0:
+            item.quality -= 1
+        item.sell_in -= 1
+        if item.sell_in < 0 < item.quality:
+            item.quality -= 1
+
+    def _update_aged_brie(self, item):
+        """Update quality and sell-in for Aged Brie."""
+        if item.quality < 50:
+            item.quality += 1
+        item.sell_in -= 1
+        if item.sell_in < 0 and item.quality < 50:
+            item.quality += 1
+
+    def _update_backstage_passes(self, item):
+        """Update quality and sell-in for Backstage Passes."""
+        if item.quality < 50:
+            item.quality += 1
+            if item.sell_in < 11 and item.quality < 50:
+                item.quality += 1
+            if item.sell_in < 6 and item.quality < 50:
+                item.quality += 1
+        item.sell_in -= 1
+        if item.sell_in < 0:
+            item.quality = 0
 
 
 class Item:
