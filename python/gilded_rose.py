980ac3ce45dcee7a7f328281f5dee03fa812c5e7
Sudhanshu <sudhanshuloshali007@gmail.com>
2025-03-22 10:22:40 +0530

    Added docstrings and extracted item names as class constants for consistency and maintainability

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,33 +1,40 @@
 # -*- coding: utf-8 -*-
 
-class GildedRose(object):
+class GildedRose:
+    """Manages a list of items and updates their quality and sell-in values daily."""
+
+    AGED_BRIE = "Aged Brie"
+    BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
+    SULFURAS = "Sulfuras, Hand of Ragnaros"
 
     def __init__(self, items):
+        """Initialize with a list of items."""
         self.items = items
 
     def update_quality(self):
+        """Update quality and sell-in values for all items based on their type."""
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+            if item.name != self.AGED_BRIE and item.name != self.BACKSTAGE_PASSES:
                 if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
+                    if item.name != self.SULFURAS:
                         item.quality = item.quality - 1
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if item.name == self.BACKSTAGE_PASSES:
                         if item.sell_in < 11:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
                         if item.sell_in < 6:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
+            if item.name != self.SULFURAS:
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                if item.name != self.AGED_BRIE:
+                    if item.name != self.BACKSTAGE_PASSES:
                         if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
+                            if item.name != self.SULFURAS:
                                 item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
